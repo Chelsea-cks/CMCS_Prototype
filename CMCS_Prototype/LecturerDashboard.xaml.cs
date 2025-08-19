3aac6ba5990acf7ef2a02c29dc00f2a09a0021e0
Chelsea-cks <st10398698@rcconnect.edu.za>
2025-08-19 11:40:35 +0200

    Added LecturerDashboard with basic UI buttons and navigation placeholder

---
+++ CMCS_Prototype/LecturerDashboard.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace CMCS_Prototype
+{
+  
+    public partial class LecturerDashboard : Window
+    {
+        public LecturerDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void CoordinatorButton_Click(object sender, RoutedEventArgs e)
+        {
+            CoordinatorDashboard cd = new CoordinatorDashboard();
+            cd.Show();
+            this.Close();
+        }
+    }
+}
